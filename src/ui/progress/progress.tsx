e4201a9962542fa4089201d789c8b9d5160e6c5a
Abylay Aiyp <ablyaip@gmail.com>
2022-06-01 18:50:15 +0600

    feat: layout contracts complete

--- src/ui/progress/progress.tsx
+++ src/ui/progress/progress.tsx
@@ -1,5 +1,5 @@
 export const Progress = ({ value }: { value: number }) => {
-  const percent = Math.round(value / 10);
+  const percent = Math.round(value / 10) + 2;
   return (
     <div className="flex flex-row space-x-2 items-center w-full">
       <div className="border-2 border-primary rounded-full w-11/12 py-1 px-1">
